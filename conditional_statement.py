42329cdc9994322429a72f13cb16c8adbc280cbf
RaginiDighe12 <raginidighe21@gmail.com>
2026-01-22 14:19:05 +0530

    extra code added

---
+++ conditional_statement.py
@@ -0,0 +1,86 @@
+#Basica level question
+
+# Q1.Write a program to check whether number is even or odd
+
+# num=int(input("Enter the number:-"))
+# if num%2==0:
+#     print(f"{num} is even.")
+# else:
+#     print(f"{num} is odd.")
+
+
+#Q2.Write a program to check whether a number is positive or negative
+
+# num=int(input("Enter the number:-"))
+# if num>0:
+#     print("Num is Positive.")
+# else:
+#     print("Num is Negative.")
+
+
+#Q3.Write a program to check whether a person is eligible to vote (age ≥ 18)
+
+# age=int(input("Enter the age:-"))
+# if age>=18:
+#     print("person can vote")
+# else:
+#     print("person cannot vote")
+
+#Q4.Write a program to check whether a given character is a vowel or consonant.
+
+# ch=input("Enter the character:-").lower()
+
+# if ch in "aeiou":
+#     print("vowels")
+# else:
+#     print("consonants")
+
+#Q5.Write a program to check whether a student is pass or fail (pass marks = 35)
+
+# marks=int(input("Enter the marks:-"))
+# if marks>=35:
+#     print("Pass in exam.")
+# else:
+#     print("Fail in exam")
+
+
+#Medium level question
+
+#Q1.Write a program to find the largest of two numbers.
+
+# num1=int(input("Enter the num1:-"))
+# num2=int(input("Enter the num2:-"))
+# if num1>num2:
+#     print("num1 is greater then num2")
+# else:
+#     print("num2 is greater than num1")
+
+#Q3.Write a program to check whether a number lies between 1 and 100
+
+
+# num=int(input("Enter the number:-"))
+# if num>=1 and num<=100:
+#     print("Number lies between 1 and 100")
+# else:
+#     print("Number is not lies between 1 and 100")
+
+#Q4.Write a program to check username and password.
+
+# username=input("Enter the name:-").lower()
+# password=int(input("Enter the password:-"))
+
+# if username=="ragini" and password==123:
+#     print("we can login into page")
+# else:
+#     print("We cannot login into page")
+
+#Q5.Write a program to check whether a number is divisible by 5 or 11
+
+# num=int(input("Enter the number:-"))
+# if num
+
+# i=1
+
+# while(i<=10):
+#     print(i)
+#     i+=1
